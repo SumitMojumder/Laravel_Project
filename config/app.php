3f672cd02c3bf3d3d15c3507f2bdc7da8ad7b75d
Sumit Mojumder <sumit15-13784@diu.edu.bd>
2025-04-21 18:18:39 +0600

    directory structure

--- config/app.php
+++ config/app.php
@@ -11,7 +11,7 @@ return [
     | framework needs to place the application's name in a notification or
     | other UI elements where an application name needs to be displayed.
     |
-    */
+    */ 
 
     'name' => env('APP_NAME', 'Laravel'),
 
